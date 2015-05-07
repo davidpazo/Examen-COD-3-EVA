61d80fa6db89798c6b965f62a78085997acefef8
dpazolopez <dpazolopez@atanasoft07.danielcastelao.org>
2015-05-07 10:11:33 +0200

    Commit final

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -8,44 +8,44 @@ import java.util.Scanner;
  * @author damian editado por dpazolopez ;)
  */
 public class Superficies {
-    
+
     private static Scanner sc;
 
     public static void main(String[] args) {
         Modelo mod = new Modelo();
         Vista obj = new Vista();
         obj.Introducir(mod);
-
+        //calcula area del cuadrado
         if (mod.getShapeType().equalsIgnoreCase("Cuadrado")) {
             //add area calculations for square
             obj.Cuadrado(mod);
 
-            mod.setSquareArea(mod.getSideLength()*mod.getSideLength());
+            mod.setSquareArea(mod.getSideLength() * mod.getSideLength());
             obj.impr(mod.getSquareArea());
-            
 
+        //calcula area del rectangulo
         }
         if (mod.getShapeType().equalsIgnoreCase("Rectangulo")) {
             //add area calculations for rectangle here
             obj.Rectangulo(mod);
 
-            mod.setRectangleArea(mod.getSideLength()*mod.getSideHeight());
+            mod.setRectangleArea(mod.getSideLength() * mod.getSideHeight());
             obj.impr(mod.getRectangleArea());
-
+        //calcular area del triangulo
         }
         if (mod.getShapeType().equalsIgnoreCase("Triangulo")) {
             //add area calculations for triangle here
             obj.Triangulo(mod);
 
-            mod.setTriangleArea((float)(0.5*mod.getBaseLength()*mod.getHeight()));
+            mod.setTriangleArea((float) (0.5 * mod.getBaseLength() * mod.getHeight()));
             obj.impr(mod.getTriangleArea());
-
+        //calcular area del circulo
         }
         if (mod.getShapeType().equalsIgnoreCase("Circulo")) {
             //add area calculations for a circle here
             obj.Circulo(mod);
 
-            mod.setCircleArea((float)(3.141592*mod.getRadius()*mod.getRadius()));
+            mod.setCircleArea((float) (3.141592 * mod.getRadius() * mod.getRadius()));
             obj.impr(mod.getCircleArea());
 
         }
--- src/superficies/Vista.java
+++ src/superficies/Vista.java
@@ -24,30 +24,30 @@ public class Vista {
         System.out.println("Has elegido: " + mod.shapeType);
 
     }
-
+    //nos da el area de nuestra figura
     public void impr(float area) {
         System.out.println("El area de tu figura es: " + area);
     }
-
+    //pedimos area del cuadrado
     public void Cuadrado(Modelo mod) {
         System.out.println("Cual es el tamaño del lado?");
         mod.setSideLength(sc.nextFloat());
     }
-
+    //pedimos area del rectangulo
     public void Rectangulo(Modelo mod) {
         System.out.println("Cual es la base del rectangulo?");
         mod.setSideLength(sc.nextFloat());
         System.out.println("Cual es la altura del rectangulo?");
         mod.setSideHeight(sc.nextFloat());
     }
-
+    //pedimos datos del triangulo
     public void Triangulo(Modelo mod) {
         System.out.println("Cual es la base del triangulo?");
         mod.setBaseLength(sc.nextFloat());
         System.out.println("Cual es la altura del triangulo?");
         mod.setHeight(sc.nextFloat());
     }
-
+    //pedimos datos del circulo
     public void Circulo(Modelo mod) {
         System.out.println("Cual es el radio del circulo?");
         mod.setRadius(sc.nextFloat());
